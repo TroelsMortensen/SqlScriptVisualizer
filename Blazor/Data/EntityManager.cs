f0030cf2acee2488c3ec37e1d3c459dac1795be6
TRMO <tromortensen@hotmail.com>
2023-12-25 19:52:48 +0100

    organizer initial

--- Blazor/Data/EntityManager.cs
+++ Blazor/Data/EntityManager.cs
@@ -7,6 +7,11 @@ public class EntityManager(SqliteParser parser, EntityPlacementOrganizer organiz
 {
     public List<EntityViewModel> Entities { get; set; } = new();
 
+    private const int EntityHeaderHeight = 35;
+    private const int EntityAttributeHeight = 25;
+    private const int EntitySpacingX = 40;
+    private const int EntitySpacingY = 150;
+    
 
     public void GenerateData(string script)
     {
@@ -19,10 +24,40 @@ public class EntityManager(SqliteParser parser, EntityPlacementOrganizer organiz
 
     private List<EntityViewModel> ConvertToViewModels(List<List<Entity>> placements)
     {
-        throw new NotImplementedException();
+        int x = 0;
+        int y = 0;
+        List<EntityViewModel> result = new();
+        foreach (List<Entity> column in placements)
+        {
+            foreach (Entity entity in column)
+            {
+                EntityViewModel evm = ConvertEntity(entity, x, y);
+                result.Add(evm);
+                x = UpdateXCoordinate(x, entity);
+            }
+
+            y += EntitySpacingY;
+        }
+
+        return result;
     }
 
-    
+    private static int UpdateXCoordinate(int x, Entity entity)
+    {
+        x += EntityHeaderHeight;
+        x += entity.Attributes.Count * EntityAttributeHeight;
+        x += EntitySpacingX;
+        return x;
+    }
 
-    
+    private static EntityViewModel ConvertEntity(Entity entity, int x, int y)
+    {
+        EntityViewModel evm = new()
+        {
+            Entity = entity,
+            Xstart = x,
+            Ystart = y
+        };
+        return evm;
+    }
 }
