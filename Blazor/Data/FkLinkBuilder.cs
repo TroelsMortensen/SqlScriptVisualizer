df097dff0ce4c3c9ac1337c222a784010bcbc5de
TRMO <tromortensen@hotmail.com>
2023-12-30 16:18:07 +0100

    move method to separate class

---
+++ Blazor/Data/FkLinkBuilder.cs
@@ -0,0 +1,44 @@
+﻿using Blazor.ViewModels;
+using Attribute = Blazor.Data.Models.Attribute;
+
+namespace Blazor.Data;
+
+public class FkLinkBuilder
+{
+    public List<FkLink> Build(List<EntityViewModel> entities)
+    {
+        List<FkLink> links = new();
+        foreach (EntityViewModel entityVm in entities)
+        {
+            for (var index = 0; index < entityVm.Entity.Attributes.Count; index++)
+            {
+                Attribute attr = entityVm.Entity.Attributes[index];
+                if (attr.ForeignKey == null) continue;
+
+                int sourceY = CalcSourceY(index);
+                int sourceX = 0;
+                EntityViewModel targetVm = FindTargetEntityViewModel(entities, attr.ForeignKey!.TargetTableName);
+                int targetX = Constants.EntityBoxWidth;
+                int targetY = CalcTargetY(targetVm, attr.ForeignKey.TargetAttributeName);
+                FkLink fkl = new(entityVm, targetVm, (sourceX, sourceY), (targetX, targetY));
+                links.Add(fkl);
+            }
+        }
+
+        return links;
+    }
+
+    private static int CalcSourceY(int index)
+        => Constants.EntityHeaderHeight + (1 + index) * Constants.EntityAttributeHeight +
+           Constants.EntityAttributeHeight / 4; // why magic 4? Thought it would be 2.
+
+    private static int CalcTargetY(EntityViewModel targetVm, string fkAttr)
+    {
+        Attribute attribute = targetVm.Entity.Attributes.Single(attr => attr.Name.Equals(fkAttr));
+        int idx = targetVm.Entity.Attributes.IndexOf(attribute) + 1;
+        return Constants.EntityHeaderHeight + idx * Constants.EntityAttributeHeight + Constants.EntityAttributeHeight / 4;
+    }
+
+    private static EntityViewModel FindTargetEntityViewModel(List<EntityViewModel> entities, string foreignKeyTargetTableName)
+        => entities.Single(evm => evm.Entity.Name.Equals(foreignKeyTargetTableName));
+}
